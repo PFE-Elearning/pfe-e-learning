366421f0fbe4dc8a0b6b6e561ff9503aa13303f1
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-19 22:16:52 +0100

    last version of courses section

--- src/components/Courses-section/Courses.js
+++ src/components/Courses-section/Courses.js
@@ -1,59 +1,50 @@
-import react from 'react';
+import react, { useState } from 'react';
 import {Container,Row,Col} from 'reactstrap';
 import './courses.css';
+import CourseCard from './CourseCard';
 import CourseImg1 from '../../assets/images/web-design.png';
 import CourseImg2 from '../../assets/images/graphics-design.png';
 import CourseImg3 from '../../assets/images/ui-ux.png';
-function Courses(){
+const Courses=()=>{
+    const [courses,setCourses]=useState([
+        {
+            id:1,
+            imgUrl:CourseImg1,
+            title:'Php Course BootCamp',
+            lesson:10,
+            students:12.3,
+            rating:4.5
+        },
+        {
+            id:2,
+            imgUrl:CourseImg2,
+            title:'Javascript Course BootCamp',
+            lesson:12,
+            students:12.3,
+            rating:4.5
+        },
+        {
+            id:3,
+            imgUrl:CourseImg3,
+            title:'Python Course BootCamp',
+            lesson:9,
+            students:12.3,
+            rating:4.5
+        },
+    ]);
     return (
         <section>
             <Container>
                 <Row>
-                <Col lg='4'>
-                    <div className='single__course__item'>
-                        <div className='course__img' >
-                        <img src={CourseImg1} alt='' className='w-100' />
-                        </div>
-                    
-                    <div className='course__details '>
-                        <h5 className='course__title mb-4'>
-                                Web Design BootCamp 2023 for Beginers
-                        </h5>
-                        <div 
-                        className='d-flex justify-content-between align-items-center'
-                        >
-                            <p 
-                            className='lesson d-flex align-items-center gap-1'
-                            >
-                                <i className='ri-book-open-line'></i>
-                                12 Lessons
-                            </p>
-                            <p 
-                            className='students d-flex align-items-center gap-1'
-                            >
-                                <i className='ri-user-line'></i>
-                                12.5k
-                            </p>
-                        </div>
-                        <div 
-                        className='d-flex justify-content-between align-items-center'
-                        >
-                            <p 
-                            className='rating d-flex align-items-center gap-1'
-                            >
-                                <i className='ri-star-fill'></i>
-                                5.9k
-                            </p>
-                            <p 
-                            className='enroll d-flex align-items-center gap-1'
-                            >
-                                <a href='#' >Enroll Now</a>
-                            </p>
-                        </div>
-
-                    </div>
-                    </div>
-                </Col>
+                    {
+                        courses.map(item=>{
+                            return(
+                                <Col lg='4'>
+                                <CourseCard key={item.id} item={item} />
+                                </Col>
+                            )
+                        })
+                    }
                 </Row>
             </Container>
         </section>
