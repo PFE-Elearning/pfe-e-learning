b80e32a50bf709098a1a9e49133bd916ace088bf
Yosfben <youssefbensaid222@gmail.com>
2023-06-20 23:51:37 +0100

    free courses section

--- src/components/Free-course-section/FreeCourseCard.js
+++ src/components/Free-course-section/FreeCourseCard.js
@@ -1,35 +1,30 @@
-
-
-import React from 'react'
+import React from "react";
 
 const FreeCourseCard = (props) => {
+  const { imgUrl, title, students, rating } = props.item;
 
-
-    const {imgUrl,title,students,rating,} = props.item
   return (
-    <div>
-      
-            <div className='single_free_course'>
-              <div className='free_course_image'>
-                <img src={imgUrl} alt="" className='w-100' />
-                <button type="button" className='btn free_btn'> free </button>
-              </div>
-              <div className='free_course_detail'>
-                <h6>{title} </h6>
+    <div className="single__free__course">
+      <div className="free__course__img mb-5">
+        <img src={imgUrl} alt="" className="w-100" />
+        <button className="btn free__btn">Free</button>
+      </div>
 
-                <div className="d-flex align-items-center gap-5">
-                  <span className="d-flex align-items-center gap-2">
-                    <i class="ri-user-line"></i>
-                   {students}K</span>
-                  <span className="d-flex align-items-center gap-2">
-                    <i class="ri-star-fill"></i> {rating}k
-                  </span>
-                </div>
-              </div>
-            </div>
-         
+      <div className="free__course__details">
+        <h6>{title}</h6>
+
+        <div className=" d-flex align-items-center gap-5">
+          <span className=" d-flex align-items-center gap-2">
+            <i class="ri-user-line"></i> {students}k
+          </span>
+
+          <span className=" d-flex align-items-center gap-2">
+            <i class="ri-star-fill"></i> {rating}k
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FreeCourseCard
+export default FreeCourseCard;
