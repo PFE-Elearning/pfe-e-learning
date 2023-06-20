f770ef7bb3cbd13ade576700c6c3b5930b883ced
Yosfben <youssefbensaid222@gmail.com>
2023-06-21 00:06:45 +0100

    Newsletter section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import FreeCourse from "../components/Free-course-section/FreeCourse";
 import Testimonial from "../components/Testimonial/Testimontials";
+import Newsletter from "../components/Newsletter/Newsletter";
 const Home = () => {
   return (
     <Fragment>
     <FreeCourse />
     <Testimonial />
-    
+    <Newsletter />
+
       
     </Fragment>
   );
