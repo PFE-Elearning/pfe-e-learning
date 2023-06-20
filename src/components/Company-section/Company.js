1b21458e208fce77fc9d8b1c9165d48629504fa5
mustaphaKENBOUCH <mosken666@gmail.com>
2023-06-20 13:26:47 +0100

    company section

--- src/components/Company-section/Company.js
+++ src/components/Company-section/Company.js
@@ -5,42 +5,50 @@ import { Container, Row, Col } from 'reactstrap'
 const Company = () => {
   return (
     <section>
-      <Row>
-        <Col lg='2' md='3'></Col>
-          <h2>
+        <Row>
+        <Col lg='2' md='3'>
+          <h3 className='d-flex align-items-center gap-1'>
             <i className='ri-vimeo-line'></i>
             Vimeo
-          </h2>
+          </h3>
+        </Col>
+        
         <Col lg='2' md='3'>
-           <h2>
+           <h3 className='d-flex align-items-center gap-1'>
             <i className='ri-pinterest-line'></i>
             Pinterest
-          </h2>
+          </h3>
         </Col>
 
         <Col lg='2' md='3'>
-           <h2>
+           <h3 className='d-flex align-items-center gap-1'>
             <i className='ri-dribbble-line'></i>
             Dribble
-          </h2>
+          </h3>
         </Col>
 
         <Col lg='2' md='3'>
-          <i className='ri-apple-fill'></i>
-          Apple
+          <h3 className='d-flex align-items-center gap-1'>
+            <i className='ri-apple-fill'></i>
+            Apple
+          </h3>
         </Col>
 
         <Col lg='2' md='3'>
-          <i className='ri-finder-fill'></i>
-          Finder
+          <h3 className='d-flex align-items-center gap-1'>
+            <i className='ri-finder-fill'></i>
+            Finder
+          </h3>
         </Col>
 
         <Col lg='2' md='3'>
-          <i className='ri-google-fill'></i>
-          Google
+          <h3 className='d-flex align-items-center gap-1'>
+            <i className='ri-google-fill'></i>
+            Google
+          </h3>
         </Col>
 
-      </Row>
+        </Row>
     </section>
   )
 }
