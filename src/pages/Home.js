be19968af431f882016bb893f0eff2008a5f6413
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-20 01:49:14 +0100

    merge hero-section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,18 @@ import react,{Fragment} from "react";
 import Courses from "../components/Courses-section/Courses";
 import ChooseUs from "../components/Choose-us/ChooseUs";
 import Header from '../components/Header/Header'
-
-const Home = () => {
-  return (
-    <Fragment>
-      <Header />
-    </Fragment>
-  )
+import HeroSection from "../components/Hero-Section/HeroSection";
+import Features from "../components/Feature-section/Feature";
+const Home=()=>{
+    return(
+        <Fragment>
+        <Header />
+        <HeroSection />
+        <Courses />
+        <ChooseUs />
+        <Features />
+        </Fragment>
+    );
 }
 export default Home;
 
