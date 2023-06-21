78b9b9933e6076d693f3358658059403636199bc
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-21 23:12:22 +0100

    create table component

--- src/admin/Users.js
+++ src/admin/Users.js
@@ -1,6 +1,35 @@
+import { useState } from "react";
+import Table from "./compnents/Table";
+
 const Users=()=>{
+    let [data,setData]=useState([
+        {
+            'id':1,
+            'name':"hassan",
+            'prenom':"hassani",
+            'city':"agadir",
+        },
+        {
+            'id':2,
+            'name':"rachid",
+            'prenom':"hassani",
+            'city':"agadir",
+        },
+        {
+            'id':3,
+            'name':"amir",
+            'prenom':"hassani",
+            'city':"agadir",
+        },
+    ])
     return(
-        <h1>Users</h1>
+        <div className="m-4">
+            <h2 className="mt-3 mb-3">List of Users</h2>
+            <div className="w-100 d-flex justify-content-end">
+                <button className="btn btn-success rounded-2 ">Add</button>
+            </div>
+            <Table data={data} />
+        </div>
     )
 }
 export default Users;
--- src/admin/compnents/Table.js
+++ src/admin/compnents/Table.js
@@ -1,6 +1,37 @@
-const Users=()=>{
-    return(
-        <h1>Users</h1>
-    )
+import react from "react";
+
+export default function Table(props) {
+    let data=props.data;
+    return (
+        <>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">name</th>
+                        <th scope="col">prenom</th>
+                        <th scope="col">city</th>
+                        <th scope="col" colSpan={2}>actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map(element=>{
+                            return(
+                                <tr>
+                                <th scope="row">{element.id}</th>
+                                <td>{element.name}</td>
+                                <td>{element.prenom}</td>
+                                <td>{element.city}</td>
+                                <td>delete</td>
+                                <td>update</td>
+                            </tr>
+                            )
+                        })
+                    }
+                    
+                </tbody>
+            </table>
+        </>
+    );
 }
-export default Users;
