1124dc5d40e65bc3a08db141220f23de8ae862a8
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-22 00:59:36 +0100

    crate admin users page

--- src/admin/Users.js
+++ src/admin/Users.js
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 import Table from "./compnents/Table";
 import axios from "axios";
-const Users=()=>{
-    let [data,setData]=useState([]);
-    let [headings,setHeadings]=useState([]);
-    useEffect(()=>{
+const Users = () => {
+    let [data, setData] = useState([]);
+    let [headings, setHeadings] = useState([]);
+    
+    useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/users')
-        .then(json=>{
-            setData(json.data.data);
-            setHeadings(Object.keys(data[0]));
-        })
-    },[]);
-    return(
+            .then(response => {
+                const jsonData = response.data.data;
+                setData(jsonData);
+                if (jsonData.length > 0) {
+                    const keys = Object.keys(jsonData[0]);
+                    setHeadings(keys);
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+    return (
         <div className="m-4">
             <h2 className="mt-3 mb-3">List of Users</h2>
             <div className="w-100 d-flex justify-content-end">
                 <button className="btn btn-success rounded-2">Add</button>
             </div>
             <div className="mt-5">
-            <Table data={data} headings={headings}/>
+                <Table data={data} headings={headings} />
             </div>
         </div>
     )
