6652ce5a85ae47a2dffa8e4984742f999334a62f
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-22 05:21:25 +0100

    add user functionality

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -1,6 +1,74 @@
-const Dashboard=()=>{
-    return(
-        <h1>Dashboard</h1>
-    )
-}
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Table from "./compnents/Table";
+import axios from "axios";
+
+const Dashboard = () => {
+    let [data, setData] = useState([]);
+    let [headings, setHeadings] = useState([]);
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/blogs')
+            .then(response => {
+                console.log(response.data)
+                const jsonData = response.data.data;
+                setData(jsonData);
+                if (jsonData.length > 0) {
+                    const keys = Object.keys(jsonData[0]);
+                    setHeadings(keys);
+                    // Perform actions with the keys and data
+                }
+            })
+            .catch(error => {
+                // Handle any errors that occur during the API request
+                console.error(error);
+            });
+    }, []);
+    const handleDelete=()=>{}
+    const handleUpdate=()=>{}
+  return (
+    <div className="content m-4">
+      <div className="d-flex justify-content-between">
+       { [1,2,3].map(element=>{
+            return (
+      <div className="">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="">
+            <div className="card card-chart">
+              <div className="card-header card-header-success">
+                <div className="ct-chart" id="dailySalesChart"></div>
+              </div>
+              <div className="card-body">
+                <h4 className="card-title">Daily Sales</h4>
+                <p className="card-category">
+                  <span className="text-success">
+                    <i className="fa fa-long-arrow-up"></i> 55%{" "}
+                  </span>{" "}
+                  increase in today sales.
+                </p>
+              </div>
+              <div className="card-footer">
+                <div className="stats">
+                  <i className="material-icons">access_time</i> updated 4
+                  minutes ago
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ... */}
+          {/* Rest of the HTML structure */}
+          {/* ... */}
+        </div>
+      </div>
+      </div>)})}
+      </div>
+      <div className="container mt-4">
+      <Table data={data} headings={headings} handleDelete={handleDelete} 
+                handleUpdate={handleUpdate}
+       />
+      </div>
+    </div>
+  );
+};
+
 export default Dashboard;
--- src/admin/Settings.js
+++ src/admin/Settings.js
@@ -1,6 +1,122 @@
-const Settings=()=>{
-    return(
-        <h1>Settings</h1>
-    )
-}
+import React, { useState } from 'react';
+
+const Settings = () => {
+  const [username, setUsername] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleFileUpload = (e) => {
+    // Handle file upload logic here
+  };
+
+  const updateProfile = (e) => {
+    e.preventDefault();
+
+    // Perform form validation and save/update logic here
+
+    // Reset form fields
+    setUsername('');
+    setFname('');
+    setLname('');
+    setEmail('');
+    setNewPassword('');
+    setConfirmPassword('');
+  };
+
+  return (
+    <div className='container'>
+        <div className="mt-4">
+      <div className="card-body">
+        <div className="d-flex flex-row align-items-center mb-15">
+          <div className="d-flex flex-column h-80px px-10 justify-content-around">
+            <div className="fw-bold d-flex align-items-center fs-5">
+            </div>
+            <a href="#" className="fw-semobold text-muted text-hover-info fs-7">
+              {/* {userdata.email} */}
+            </a>
+          </div>
+        </div>
+        <form onSubmit={updateProfile}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="form-control"
+              id="username"
+            />
+          </div>
+          <div className="row mb-3">
+            <div className="col">
+              <label htmlFor="fname" className="form-label">First Name</label>
+              <input
+                type="text"
+                value={fname}
+                onChange={(e) => setFname(e.target.value)}
+                className="form-control"
+                id="fname"
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="lname" className="form-label">Last Name</label>
+              <input
+                type="text"
+                value={lname}
+                onChange={(e) => setLname(e.target.value)}
+                className="form-control"
+                id="lname"
+              />
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="email"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="new-password" className="form-label">New password</label>
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="form-control"
+              id="new-password"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="confirm-password" className="form-label">Confirm password</label>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="form-control"
+              id="confirm-password"
+            />
+          </div>
+          <div className="mb-15">
+            <label htmlFor="about" className="form-label">About</label>
+            <textarea className="form-control" id="about"></textarea>
+          </div>
+          <div className="d-flex flex-row justify-content-end mt-3">
+            <div className="w-175px d-flex flex-row justify-content-between">
+              <button type="reset" className="btn bg-danger btn-danger mx-5">Reset</button>
+              <button type="submit" className="btn btn-info">Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
 export default Settings;
