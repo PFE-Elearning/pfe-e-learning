25e34532c72638e2175df9c945778a2456a7eb64
mustaphaKENBOUCH <mosken666@gmail.com>
2023-06-22 01:30:51 +0100

    last version

--- src/admin/compnents/Table.js
+++ src/admin/compnents/Table.js
@@ -6,38 +6,36 @@ export default function Table(props) {
     return (
         <>
             <table class="table">
-              <thead>
-                <tr>
-                  {
-                    headings.map(element => {
-                      return (
-                        <th scope="col" className="text-center">{element}</th>
-                      )
-                    })
-                  }
-                    <th scope="col" className="text-center">actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  data.map(element => {
-                    return (
-                      <tr>
-                        <th scope="row">{element.id}</th>
-                        <td className="text-center">{element.first_name}</td>
-                        <td className="text-center">{element.last_name}</td>
-                        <td className="text-center">{element.email}</td>
-                        <td className="text-center">{element.phone}</td>
-                        <td className="text-center">{element.role}</td>
-                        <td className="text-center">
-                          <i className="mx-5"><i className="fa-solid fa-trash text-danger"></i></i>
-                          <i className="fa-solid fa-pen-to-square text-primary"></i>
-                        </td>
-                      </tr>
-                    )
-                  })
-                }
-              </tbody>
+                <thead>
+                    <tr>
+                        {
+                            headings.map(element => {
+                                return (
+                                    <th scope="col" className="text-center">{element}</th>
+                                )
+                            })
+                        }
+                        <th scope="col" className="text-center">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((element, index) => (
+            <tr key={index}>
+              {headings.map((heading, index) => (
+                <td key={index} className="text-center">
+                  {element[heading]}
+                </td>
+              ))}
+              <td className="text-center">
+                <i className="mx-5">
+                  <i className="fa-solid fa-trash text-danger"></i>
+                </i>
+                <i className="fa-solid fa-pen-to-square text-primary"></i>
+              </td>
+            </tr>
+          ))}
+
+                </tbody>
             </table>
         </>
     );
